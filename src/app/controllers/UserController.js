c8830749871c0faff7ed5dc9becd80911a34e0eb
Danilo Parisotto <danilo@amirainfo.com.br>
2019-11-06 14:41:57 -0300

    Fix - UserConontroller.js req,res

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,7 @@
 import User from '../models/User';
 
 class UserController {
-  async store(res, req) {
+  async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
